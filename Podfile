4668821e063f1add07c0afb32281ec63f3becac3
MHTmikiya <m.okugawa@mht-code.com>
2019-06-29 02:17:06 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SeminorRxSample' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SeminorRxSample
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'Alamofire'
+  pod 'ObjectMapper'
+  target 'SeminorRxSampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SeminorRxSampleUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ SeminorRxSample/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  SeminorRxSample
+//
+//  Created by がーたろ on 2019/06/29.
+//  Copyright © 2019 がーたろ. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+import Alamofire
+
+
+class ViewController: UIViewController {
+
+    private let baseUrl: String =  "http://zipcloud.ibsnet.co.jp/api/search"
+    @IBOutlet var zipcodeTxt: UITextField!
+    @IBOutlet var tableView: UITableView!
+    
+    
+    private let disposeBag = DisposeBag()
+    let loadFinishTrigger: PublishSubject<Void> = PublishSubject<Void>()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        httpRequest()
+    }
+    
+    
+    
+    
+    func httpRequest() {
+//    func httpRequest(zipcodeTxt: UITextField) {
+//        zipcodeTxt.rx.text.subscribe({ _ in
+            let url = self.baseUrl
+            let headers: HTTPHeaders = [
+                "Contenttype": "application/json"
+            ]
+            let parameters:[String: Int] = [
+                "zipcode": 5600003
+            ]
+            Alamofire.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+                if let result = response.result.value as? [String: Any] {
+                    print(result)
+//                    self.loadFinishTrigger.onNext(())
+                }
+            }
+//        }).disposed(by: disposeBag)
+    }
+
+
+}
+
+//extension ViewController: UITableViewDelegate,UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//    
+//    
+//}
