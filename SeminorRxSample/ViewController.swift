946db56dd7eb40849c6bac0e314dba053093288a
MHTmikiya <m.okugawa@mht-code.com>
2019-06-29 13:26:39 +0900

    文字数制限、数字のみ入力に修正

--- SeminorRxSample/ViewController.swift
+++ SeminorRxSample/ViewController.swift
@@ -16,26 +16,22 @@ import SwiftyJSON
 class AddressModel: Mappable {
     required init?(map: Map) {
     }
-
     var resultList: [Result] = []
     func mapping(map: Map) {
         resultList <- map["resultList"]
     }
-
 }
 
 
 class Result: Mappable {
     required init?(map: Map) {
     }
-
     var address1: String = ""
     var address2: String = ""
     var address3: String = ""
     var kana1: String = ""
     var kana2: String = ""
     var kana3: String = ""
-
     func mapping(map: Map) {
         address1 <- map["address1"]
         address2 <- map["address2"]
@@ -53,17 +49,14 @@ class ViewController: UIViewController {
 
     private let baseUrl: String =  "http://zipcloud.ibsnet.co.jp/api/search?zipcode="
     @IBOutlet var zipcodeTxt: UITextField!
-    @IBOutlet var tableView: UITableView!
-    
     var textLength = BehaviorRelay<Int>(value: 0)
-
     private let disposeBag = DisposeBag()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         httpRequest(zipcodeTxt: zipcodeTxt)
         limitLength(textField: zipcodeTxt)
+        onlyNumber(textField: zipcodeTxt)
     }
     
     func limitLength(textField: UITextField) {
@@ -74,10 +67,15 @@ class ViewController: UIViewController {
             }
         }).disposed(by: disposeBag)
     }
+    
+    func onlyNumber(textField: UITextField) {
+        // 数字のみの入力制限
+        textField.rx.text.subscribe(onNext: { text in
+            guard let txt = textField.text else { return }
+            guard let intText = Int(txt) else { textField.text = ""; return }
+        }).disposed(by: disposeBag)
+    }
 
-    
-    
-//    func httpRequest() {
     func httpRequest(zipcodeTxt: UITextField) {
         zipcodeTxt.rx.text.subscribe({ _ in
             let url = self.baseUrl + zipcodeTxt.text!
@@ -89,7 +87,6 @@ class ViewController: UIViewController {
                 Alamofire.request(url, method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
                     if let json:JSON = JSON(response.result.value) {
                         print(json)
-
                         let address: [Result]? = Mapper<Result>().mapArray(JSONObject: json)
                         print(address)
                     }
@@ -97,8 +94,6 @@ class ViewController: UIViewController {
             }
         }).disposed(by: disposeBag)
     }
-
-
 }
 
 
